7c791a18d892bb6907f6215b39bdba17daa3b3e0
Satwik Mishra <satwik.mishra1995@gmail.com>
2021-07-03 22:40:30 -0400

    multiple constructors

--- src/test/Triangle.java
+++ src/test/Triangle.java
@@ -2,9 +2,24 @@ package test;
 
 public class Triangle {
 	private String type;
+	private int height;
 	
 	public Triangle(String type) {
 		this.type = type;
+	}
+	
+	public Triangle(int height) {
+		this.height = height;
+	}
+	
+	public Triangle(String type, int height) {
+		this.type=  type;
+		this.height = height;
+	}
+	
+	
+	public int getHeight() {
+		return height;
 	}
 	
 	public String getType() {
@@ -18,6 +33,6 @@ public class Triangle {
 
 
 	public void draw() {
-		System.out.println(getType() + " Triangle Drawn");
+		System.out.println(getType() + " Triangle Drawn with height " + getHeight());
 	}
 }
